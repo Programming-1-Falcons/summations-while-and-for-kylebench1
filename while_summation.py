d3dfe1c706ac30f170691751013cb883d63a7c8a
kylebench1 <benchkyl000@alpinesd.org>
2024-10-22 11:37:46 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -4,6 +4,6 @@ looped_times = 0
 total = 0
 
 while looped_times <= user_num:
-    looped_times = looped_times + 1
     total = total + looped_times
+    looped_times = looped_times + 1
 print(total)
